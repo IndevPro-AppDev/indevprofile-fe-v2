6836ca91f2f2b957e8b05ceeec123df913a1db87
yarabramasta <yarabram111@gmail.com>
2025-06-26 17:13:44 +0700

    refactor: remove unused localization and routing files; update layout and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,59 @@
-export default function RootLayout({ children }: React.PropsWithChildren) {
-  return children
+import type { Metadata } from 'next'
+
+import localFont from 'next/font/local'
+
+import Header from '~/components/header'
+import { ThemeProvider } from '~/components/theme-provider'
+import { TRPCReactProvider } from '~/lib/trpc/react'
+
+import './globals.css'
+
+import { cn } from '~/lib/utils'
+
+export const metadata: Metadata = {
+  title: 'IndevPro',
+  description: 'Together We Lead, Together We Achieve, Together We Innovate.'
+}
+
+export const calSans = localFont({
+  src: './fonts/CalSans-Regular.ttf',
+  variable: '--font-cal-sans'
+})
+
+export const coraMontserra = localFont({
+  src: './fonts/cora-montserra-variable.ttf',
+  variable: '--font-cora-montserra'
+})
+
+export const decog = localFont({
+  src: './fonts/decog.otf',
+  variable: '--font-decog'
+})
+
+export default async function RootLayout({
+  children
+}: React.PropsWithChildren) {
+  return (
+    <TRPCReactProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            calSans.variable,
+            coraMontserra.variable,
+            decog.variable
+          )}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="w-full pt-14">{children}</main>
+          </ThemeProvider>
+        </body>
+      </html>
+    </TRPCReactProvider>
+  )
 }
