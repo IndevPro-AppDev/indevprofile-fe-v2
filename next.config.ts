cb27f329e73917716c8821927713bd81f45c88e1
yarabramasta <yarabram111@gmail.com>
2025-06-26 17:17:00 +0700

    feat: add babel-plugin-react-compiler and update next.config.ts experimental settings

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
 
 const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true
+  },
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true // Ignore ESLint errors during build
