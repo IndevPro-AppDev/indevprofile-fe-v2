28a2f6a507a2b6534daf88c50fd4737b067a88ae
yarabramasta <yarabram111@gmail.com>
2025-07-08 08:49:07 +0700

    feat: 3d indevpro logo scene

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { motion } from 'motion/react'
 
+import IndevproScene from '~/components/indevpro-scene'
 import LogoMarquee from '~/components/marquee/logo-marquee'
 
 export const Route = createFileRoute('/')({
@@ -9,7 +11,21 @@ export const Route = createFileRoute('/')({
 function RouteComponent() {
   return (
     <div className="relative w-full max-w-screen overflow-x-hidden">
-      <section className="flex h-[calc(100dvh-calc(var(--spacing)*14))] items-center justify-center"></section>
+      <section className="flex h-[calc(100dvh-calc(var(--spacing)*14))] items-center justify-center">
+        <div id="3d-scene" className="relative mx-auto h-64 w-64">
+          <motion.div
+            className="absolute top-0 left-0 h-full w-full"
+            initial={{ opacity: 0, filter: 'blur(6px)' }}
+            whileInView={{ opacity: 1, filter: 'blur(0)' }}
+            exit={{ opacity: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, ease: [0.16, 1, 0.3, 1], delay: 0.15 }}
+          >
+            <IndevproScene />
+          </motion.div>
+        </div>
+      </section>
+
       <section className="h-dvh"></section>
       <section className="h-dvh"></section>
 
