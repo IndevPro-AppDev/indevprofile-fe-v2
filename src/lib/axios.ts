41862ea0667a350012ca51b10477018f6d39b60e
yarabramasta <yarabram111@gmail.com>
2025-08-07 13:15:38 +0700

    refactor(api): simplify axios setup by removing caching logic

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,5 @@
 import { TRPCError } from '@trpc/server'
 import Axios from 'axios'
-import { buildStorage, setupCache } from 'axios-cache-interceptor'
-import { del, get, set } from 'idb-keyval'
 import { match } from 'ts-pattern'
 
 const indevproApi = Axios.create({
@@ -42,22 +40,6 @@ indevproApi.interceptors.response.use(
   }
 )
 
-const idbCacheStorage = buildStorage({
-  async find(key) {
-    const value = await get(key)
-    if (!value) return
-    return JSON.parse(value)
-  },
-  async set(key, value) {
-    await set(key, JSON.stringify(value))
-  },
-  async remove(key) {
-    await del(key)
-  }
-})
-
-const cachedIndevproApi = setupCache(indevproApi, { storage: idbCacheStorage })
-
 export const api = {
-  indevpro: cachedIndevproApi
+  indevpro: indevproApi
 }
