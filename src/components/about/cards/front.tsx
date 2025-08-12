d087e6a38de10739d1fdd380f494adb3bfdd6992
yarabramasta <yarabram111@gmail.com>
2025-08-12 08:27:18 +0700

    fix(cards): remove period name from member department display

--- src/components/about/cards/front.tsx
+++ src/components/about/cards/front.tsx
@@ -39,7 +39,7 @@ export default function MemberCardFront({
       <div className="to-primary-gradient-start/60 absolute inset-x-0 bottom-0 bg-gradient-to-b from-transparent">
         <div className="relative flex h-fit w-full flex-col justify-end space-y-1 px-3 pb-2 lg:px-6 lg:py-4">
           <span className="text-xs font-medium text-white/80 drop-shadow-sm">
-            {member.department} ({member.periode.name})
+            {member.department}
           </span>
           <h3
             className={cn(
